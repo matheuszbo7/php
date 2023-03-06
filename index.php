1e0fd0b7d0581d10755cf9ab7b4385b18f85e1c6
MATHEUS ZANONI BITTENCOURT DE OLIVEIRA <oliveira.4942@aluno.pr.senac.br>
2023-03-06 08:41:43 -0300

    salvar

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+   
+     $nome = "aline";
+     $idade = "23";
+     $endereço = "rua das flores,316-centro";
+     $cidade = "ivaiporã";
+     $cep = "86.870.000";
+    $ps = 8;
+    $pss = 10;
+    $psss = 9;
+    $media = ($ps+$pss=$pss);
+    echo("deu certoooo");
+    echo("</br>");
+    echo("O seu nome é:") . $nome;
+    echo("</br>");
+    echo("A sua idade?") . $idade;
+    echo("</br>");
+    echo("O seu endereço?") . $endereço;
+    echo("</br>");
+    echo("Onde mora:") . $cidade;
+    echo("</br>");
+    echo("O seu CEP é") . $cep;
+    echo("</br>");
+    echo("Primeiro semestre:") . $ps;
+    echo("</br>");
+    echo("Seguando semestre:") . $pss;
+    echo("</br>");
+    echo("Terceiro semestre:") . $psss;
+    echo("</br>");
+    echo("Sua média é:") . $media;
+?>
